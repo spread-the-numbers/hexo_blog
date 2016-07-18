5efaf4d6483d358dcba48a9678af251228cd54d4
Denise <denise@omni.tt>
2016-07-18 20:17:49 +0200

    added deploy key to github

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+# deploy script
+
 eval "$(ssh-agent -s)" # Start the ssh agent
 echo "$DEPLOY_KEY" > deploy_key.pem
-chmod 600 deploy_key.pem # This key should have push access
+
+chmod 600 deploy_key.pem # Needs push access (-> git repo settings)
 ssh-add deploy_key.pem
-git remote add deploy $REPO_URI
+git remote add deploy https://github.com/spread-the-numbers/hexo_blog.git
 git push deploy master
